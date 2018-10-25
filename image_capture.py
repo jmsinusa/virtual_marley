6121f2d4894062f356d93f7954b001816ba03b6d
jmsinusa <jmsinusa@gmail.com>
2018-10-25 16:35:40 -0600

    image_capture

--- image_capture.py
+++ image_capture.py
@@ -2,4 +2,25 @@
 Capture images from webcam and other sources
 """
 
+# from cv2 import VideoCapture
+import cv2
+from datetime import datetime
+import time
+import os
 
+
+def capture_regular_imagery(base_dir, wait_time_secs=2):
+    assert os.path.isdir(base_dir), '%s not a directory' % base_dir
+    cap = cv2.VideoCapture(0)  #set the port of the camera as before
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+    while True:
+        hour_dir = datetime.now().strftime('%Y-%m-%d_%H')
+        hour_dir_path = os.path.join(base_dir, hour_dir)
+        if not os.path.isdir(hour_dir_path):
+            os.makedirs(hour_dir_path)
+        retval, image = cap.read()
+        img_filename = '%s.png' % datetime.now().strftime('%Y-%m-%d_%H:%M:%S.%f')
+        img_path = os.path.join(hour_dir_path, img_filename)
+        cv2.imwrite(img_path, image)
+        time.sleep(wait_time_secs - 0.107)
